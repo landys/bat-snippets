ce23d5673021da3c53c7ee20a0203959a6df708b
landys <landys.cn@gmail.com>
2014-05-26 23:54:30 -0400

    fix issues in build_ve.bat

--- ve/build_ve.bat
+++ ve/build_ve.bat
@@ -6,18 +6,18 @@ cd /d z:\
 
 msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VEDataModel\VEDataModel.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
 msbuild "Kernel\Projects\VisualizationEngineClientProject\ChartEngine\ChartEngine.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
-msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VECore\VECore.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VECore\VECore.vcxproj" /t:Rebuild /p:Configuration=Debug /p:Platform="Win32"
 
 rem patch to DSSDataImport.h and build DSSController
-cleartool unco Kernel\SourceCode\OneTierClient\DSSDataImport.h
+cleartool unco -rm Kernel\SourceCode\OneTierClient\DSSDataImport.h
 cleartool update -ove Kernel\SourceCode\OneTierClient\DSSDataImport.h
 cleartool co -unreserved -nc Kernel\SourceCode\OneTierClient\DSSDataImport.h
 patch Kernel\SourceCode\OneTierClient\DSSDataImport.h < "%~dp0\ve_patch.diff"
-msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\DSSController\DSSController.vcxproj" /define:DATAIMPORT_DLL_EXPORT /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\DSSController\DSSController.vcxproj" /p:DefineConstants=DATAIMPORT_DLL_EXPORT /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
 
 msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VEWinController\VEWinController.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
 msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VisualizationEngineClient\VisualizationEngineClient.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
-msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VETest\VETest.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+rem msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VETest\VETest.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
 
 rem build one tie client as well
 msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\OneTierClientApp\OneTierClientApp.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
---
+++ ve/update_build_ve.bat
@@ -0,0 +1,25 @@
+@echo off & setlocal EnableDelayedExpansion
+@rem Wang Jinde
+@rem 2014-05-27
+
+if [%1] == [] (
+	echo usage: %0 build_version_like_0067
+	exit /b 0
+)
+
+set build_path=\\WAS-CC2-TECH\cm_bld1\9.5.1000.%1\DEBUG
+if not exist "!build_path!" (
+	echo Cannot find the build in the path "!build_path!"
+	exit /b 1
+)
+
+cd /d z:\
+cleartool update -nov .
+
+xcopy "!build_path!\*" z:\ /E /C /R /I /Y
+
+cd /d "%~dp0"
+
+call onetie_env.bat
+
+call build_ve.bat
