9820acb041bd5580135d08e1a816896cf1d4c0a9
landys <landys.cn@gmail.com>
2014-05-27 03:33:54 -0400

    fix issues in build_ve.bat

--- ve/build_ve.bat
+++ ve/build_ve.bat
@@ -13,11 +13,12 @@ cleartool unco -rm Kernel\SourceCode\OneTierClient\DSSDataImport.h
 cleartool update -ove Kernel\SourceCode\OneTierClient\DSSDataImport.h
 cleartool co -unreserved -nc Kernel\SourceCode\OneTierClient\DSSDataImport.h
 patch Kernel\SourceCode\OneTierClient\DSSDataImport.h < "%~dp0\ve_patch.diff"
-msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\DSSController\DSSController.vcxproj" /p:DefineConstants=DATAIMPORT_DLL_EXPORT /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\DSSController\DSSController.vcxproj" /p:DefineConstants=DATAIMPORT_DLL_EXPORT /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32" /p:OutputPath="..\Debug_Use_Msi_COM\"
 
 msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VEWinController\VEWinController.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
-msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VisualizationEngineClient\VisualizationEngineClient.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
-rem msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VETest\VETest.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VisualizationEngineClient\VisualizationEngineClient.csproj" /t:Rebuild /p:Configuration=Debug /p:Platform="x86"
+
+msbuild "Kernel\Projects\VisualizationEngineClientProject\VisualizationEngineClient\VETest\VETest.vcxproj" /t:Rebuild /p:Configuration=Debug /p:Platform="Win32"
 
 rem build one tie client as well
-msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\OneTierClientApp\OneTierClientApp.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32"
+msbuild "Kernel\Projects\OneTierClientProject\OneTierClientApp\OneTierClientApp\OneTierClientApp.vcxproj" /t:Rebuild /p:Configuration=Debug_Use_Msi_COM /p:Platform="Win32" /p:OutputPath="..\Debug_Use_Msi_COM\"
