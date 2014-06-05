2cf98cb1b7151ecd026e30c78fbb3f62411e64f1
landys <landys.cn@gmail.com>
2014-06-05 03:29:32 -0400

    script only update ve related codes including ce in cc view

---
+++ ve/update_ve_only.bat
@@ -0,0 +1,10 @@
+@echo off & setlocal EnableDelayedExpansion
+@rem Wang Jinde
+@rem 2014-06-05
+
+cd /d z:\
+cleartool update -nov Kernel\Projects\VisualizationEngineClientProject
+cleartool update -nov Kernel\SourceCode\VisualizationEngine
+cleartool update -nov Kernel\IPhone\Chart
+
+pause
