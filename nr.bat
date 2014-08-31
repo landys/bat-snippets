04334710594bf8dc77d3038a49fbc6c4a13213b2
landys <landys.cn@gmail.com>
2014-08-31 12:39:09 +0800

    Improve codes.

--- nr.bat
+++ nr.bat
@@ -3,7 +3,8 @@
 set a=1
 for /f "delims=" %%i in ('dir /b *.jpg') do ( 
 
-  if not "%%~ni"=="%~n0" (
+rem for jpg files, actually we can also remove the "if" line below. For it cannot have the same name as the running bat file.
+  if not "%%~nxi"=="%~nx0" (
 rem %%~ni, the file name without the extension
 rem    if !a! LSS 10 (ren "%%i" "0!a!.jpg") else ren "%%i" "!a!.jpg"
     ren "%%i" "!a!.jpg" 
